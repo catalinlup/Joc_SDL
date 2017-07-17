41d82c4e12e29edf3012902691c0576a2ed84c0e
catalinlup <minecraft.lupau@gmail.com>
2017-07-17 17:47:55 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 using namespace std;
 
 SDL_Window* window;
+SDL_Renderer* renderer;
 
 int main(int argc,char*argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
     window=SDL_CreateWindow("Joc",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);
-    //renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+    cout<<"ceva"<<endl;
     SDL_Delay(5000);
     return 0;
 }
